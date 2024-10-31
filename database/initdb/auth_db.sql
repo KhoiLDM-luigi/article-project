2d3fcb690f15fb182e18d9177e0e40d7eccfdb91
KhoiLDM-luigi <khoiledangminh@gmail.com>
2024-10-31 17:32:27 +0700

    Fix incorrect database init script

--- database/initdb/auth_db.sql
+++ database/initdb/auth_db.sql
@@ -72,7 +72,7 @@ INSERT INTO oauth2_registered_client (
 VALUES (
     gen_random_uuid(), 'debug', CURRENT_TIMESTAMP, '$2a$12$fuwGsYQf8n7fTBObG6SHOuZsC/uEXzPkP0U0SnlB7dZBVQtn/mgEC', -- bcrypt: "secret"
     'Debug Client', 'client_secret_post', 'authorization_code,refresh_token', 
-    'https://oidcdebugger.com/debug', 'openid,articles.read,profile',
+    'https://oidcdebugger.com/debug', 'openid,articles.read,articles.write,profile',
     '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}', 
     '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000]}'
 );
